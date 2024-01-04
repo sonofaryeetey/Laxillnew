a517e8b16ca2345d8e877baf077815fb7a59cc43
Josh <ayiniiayi@gmail.com>
2024-01-04 18:39:23 +0000

    fixed mobile nav viewport

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -21,7 +21,7 @@ const Nav = () => {
     ]
     return (
         <nav className="nav w-full border-b md:border-0 md:static">
-            <div className=" mobile-link-wrapperitems-center px-4 max-w-screen-xl mx-auto md:flex md:px-8">
+            <div className=" mobile-link-wrapper items-center px-4 max-w-screen-xl mx-auto md:flex md:px-8">
                 <div className="flex items-center justify-between py-3 md:py-5 md:block">
                     <a href="javascript:void(0)">
                         <img
