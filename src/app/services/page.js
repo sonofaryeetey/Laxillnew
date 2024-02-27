0dbf0982b8066ece5393e9f4050847cd1affffcb
Josh <ayiniiayi@gmail.com>
2024-02-27 20:11:49 +0000

    changes on services: removed data security

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -149,6 +149,11 @@ const Services = () => {
                 <img src="images/data-analysis2.png" className='' alt="" />
               </div>
             </div>
+            <div class="custom-shape-divider-bottom-1709064439">
+    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill"></path>
+    </svg>
+</div>
           </div>
 
 
