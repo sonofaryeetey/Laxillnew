776793c28c21dbda74f4d0188eb12eae8d02c788
sonofaryeetey <79279272+sonofaryeetey@users.noreply.github.com>
2024-03-22 10:31:56 +0000

    Joshua image updated

--- src/app/About/page.js
+++ src/app/About/page.js
@@ -10,7 +10,7 @@ const About = () => {
     const team = [
 
         {
-            avatar: "url(./images/Aaron.jpg)",
+            avatar: "url(./images/Aaron.jpeg)",
             name: "Aaron Boateng",
             title: "Data Analyst(Team Lead)",
             desc: "Lorem Ipsum is simply dummy text of the printing and typesettin industry.",
@@ -38,7 +38,7 @@ const About = () => {
         },
 
         {
-            avatar: "https://images.unsplash.com/photo-1540569014015-19a7be504e3a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
+            avatar: "url(./images/Joshua.jpg)",
             name: "Joshua Aryeetey",
             title: "Data Engineer(Technical Program Manager)",
             desc: "Lorem Ipsum is simply dummy text of the printing and typesettin industry.",
