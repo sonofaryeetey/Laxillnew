540f057b254ded5807a0a3f33e493fd3760eb817
sonofaryeetey <79279272+sonofaryeetey@users.noreply.github.com>
2024-03-22 11:11:51 +0000

    obeds picture updated

--- src/app/About/page.js
+++ src/app/About/page.js
@@ -19,7 +19,7 @@ const About = () => {
             github: "javascript:void(0)"
         },
         {
-            avatar: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+            avatar: "images/Obed.jpeg",
             name: "Obed Obeng",
             title: "Business Intelligence Consultant",
             desc: "Lorem Ipsum is simply dummy text of the printing and typesettin industry.",
