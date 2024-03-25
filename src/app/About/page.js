5e5f5e79c33d04b4afb6654d587643a93e918f86
sonofaryeetey <79279272+sonofaryeetey@users.noreply.github.com>
2024-03-25 10:27:53 +0000

    added jagga's pic

--- src/app/About/page.js
+++ src/app/About/page.js
@@ -28,13 +28,13 @@ const About = () => {
             github: "javascript:void(0)"
         },
         {
-            // avatar: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-            // name: "Gabriel Boateng",
-            // title: "Cybersecurity Data Analyst",
-            // desc: "Lorem Ipsum is simply dummy text of the printing and typesettin industry.",
-            // linkedin: "javascript:void(0)",
-            // twitter: "javascript:void(0)",
-            // github: "javascript:void(0)"
+            avatar: "images/Jagga.jpeg",
+            name: "Gabriel Boateng",
+            title: "Cybersecurity Data Analyst",
+            desc: "Lorem Ipsum is simply dummy text of the printing and typesettin industry.",
+            linkedin: "javascript:void(0)",
+            twitter: "javascript:void(0)",
+            github: "javascript:void(0)"
         },
 
         {
